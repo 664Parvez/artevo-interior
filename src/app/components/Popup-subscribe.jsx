bdb721448e99491faddc24240dc430c6ffac3f0a
unknown <parvezrabbi664@gmail.com>
2024-10-23 00:33:01 +0600

    update4

--- src/app/components/Popup-subscribe.jsx
+++ src/app/components/Popup-subscribe.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+"use client"
 
-const PopupSubscribe = () => {
+import React, {useState} from 'react'
+import Image from "next/image"
+import MassageStyle from "../styles/popup-massage.css"
+
+import PopupImg from "../../../public/home/beloved.webp"
+
+import { FaLocationArrow } from "react-icons/fa6";
+import { LuSparkles } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
+
+const PopupSubscribe = (props) => {
+
+    const [showPopup, setShowPopup] = useState(false)
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div id={`${props.getId}`}>
+                <RxCross2 id='cross' onClick={() => setShowPopup(!showPopup)} />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-2"></div>
+                        <div className="col-lg-8">
+                            <div className="popup_content">
+                                <div className="d-flex justify-content-center align-items-center gap-4">
+                                    <div>
+                                        <h2>Our Newsletter</h2>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti ratione explicabo veniam id dolore suscipit debitis repellendus tenetur. Nihil, dolore.</p>
+                                        <div className="mt-4">
+                                            <div className="input-group">
+                                                <input type="text" placeholder='Enter your email' className='form-control form-control-lg' />
+                                                <button className='btn btn-lg'><FaLocationArrow /></button>
+                                            </div>
+                                        </div>
+                                        <ul className='mt-5'>
+                                            <li><LuSparkles /> Interior Inspiration</li>
+                                            <li><LuSparkles /> Exclusive Discount</li>
+                                            <li><LuSparkles /> How to and room reveals</li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <Image src={PopupImg} width={0} height={0} loading='lazy' alt=''/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-2"></div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 "use client"
 
+import React, {useState, useEffect} from 'react'
 import Image from "next/image"
 import Link from "next/link"
 import homeStyle from "./page.module.css"
+
+// Components
+import PopupMassage from "./components/Popup-subscribe"
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -11,6 +15,9 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { MdArrowOutward } from "react-icons/md"
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa"
 import { LuMoveRight, LuMoveLeft } from "react-icons/lu"
+import { FaLocationArrow } from "react-icons/fa6";
+import { LuSparkles } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
 
 
 // Import Swiper styles
@@ -58,11 +65,56 @@ import Project3 from "../../public/home/project3.webp"
 import Project4 from "../../public/home/project4.webp"
 import Project5 from "../../public/home/project5.webp"
 import Project6 from "../../public/home/project6.webp"
+import PopupImg from "../../public/home/beloved.webp"
 
 
 export default function Home() {
+
+  const [showPopup, setShowPopup] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowPopup(true)
+    }, 4000)
+  }, [])
+
   return (
     <>
+
+<div id={showPopup ? "formPopup" : "formPopupnone"}>
+  <RxCross2 id='cross' onClick={() => setShowPopup(!showPopup)} />
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-2"></div>
+            <div className="col-lg-8">
+                <div className="popup_content">
+                    <div className="d-flex justify-content-center align-items-center gap-4">
+                        <div>
+                            <h2>Our Newsletter</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti ratione explicabo veniam id dolore suscipit debitis repellendus tenetur. Nihil, dolore.</p>
+                            <div className="mt-4">
+                                <div className="input-group">
+                                    <input type="text" placeholder='Enter your email' className='form-control form-control-lg' />
+                                    <button className='btn btn-lg'><FaLocationArrow /></button>
+                                </div>
+                            </div>
+                            <ul className='mt-5'>
+                                <li><LuSparkles /> Interior Inspiration</li>
+                                <li><LuSparkles /> Exclusive Discount</li>
+                                <li><LuSparkles /> How to and room reveals</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <Image src={PopupImg} width={0} height={0} loading='lazy' alt=''/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-2"></div>
+        </div>
+    </div>
+</div>
+
       <section>
         <div id={homeStyle.hero_section}>
           <div className="container">
